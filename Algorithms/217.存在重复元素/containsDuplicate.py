e12d3fb3a2f380a613ddd94608c62d9f781c1367
ZZy979 <979481894@qq.com>
2020-12-13 12:40:13 +0800

    修改217.存在重复元素

--- Algorithms/217.存在重复元素/containsDuplicate.py
+++ Algorithms/217.存在重复元素/containsDuplicate.py
@@ -1,9 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen.add(num)
-        return False
+        return len(set(nums)) < len(nums)
